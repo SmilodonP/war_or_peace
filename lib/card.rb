897205f01b50ad5bdfdec836ddd4afb8b7b7cd72
Karl Fallenius <smilodon.p@gmail.com>
2024-05-12 10:47:37 -0600

    add card rank to deck

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,29 @@
 class Card
 
-    attr_reader :suit, :value, :rank
+  attr_reader :suit, :value, :rank
 
-    def initialize(suit, value, rank)
-        @suit = suit
-        @value = value
-        @rank = rank
-    end
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
 
-    
+  # def card1
+  #   suit = diamond
+	# 	value = 'Queen'
+	# 	rank = 12
+  # end
+
+	# def card2
+	# 	suit = spade
+	# 	value = '3'
+	# 	rank = 3
+	# end
+
+	# def card3
+	# 	suit = heart
+	# 	value = 'Ace'
+	# 	rank = 14
+	# end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,6 +2,42 @@ require 'rspec'
 require './lib/card'
 require './lib/deck'
 
-Rspec.describe Deck do
-    
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Deck do
+  it "has cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+
+  it "has cards with ranks" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "has high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+  end
+
 end
