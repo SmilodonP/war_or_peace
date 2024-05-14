ab9d1081498074e4347c8001bb2fbbdab3e8edf9
Karl Fallenius <smilodon.p@gmail.com>
2024-05-14 15:21:01 -0600

    working up to pile_cards

---
+++ lib/turn.rb
@@ -0,0 +1,61 @@
+class Turn
+require 'pry'
+	attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type 
+		if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+			@type = :basic
+		elsif deck1.rank.at(0) == deck2.rank.at(0) 
+			@type = :war
+		else
+			@type = :mutually_assured_destruction
+		end
+  end
+
+	def winner
+		if @type == :basic
+			if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) 
+				winner = player1
+			else
+				winner = player2
+			end
+
+		elsif @type == :war 
+			if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+				winner = player1
+			else
+				winner = player2
+			end
+		
+		else #@type == :mutually_assured_destruction
+			"Everybody Loses & Nobody Wins!"
+		end
+	end
+
+	def pile_cards
+		if type == :basic
+			spoils_of_war << player1.deck.remove_card
+			spoils_of_war << player2.deck.remove_card  
+		end
+
+		if type == :war
+			player1.deck.rank(0,1,2) << spoils_of_war
+			player2.deck.rank(0,1,2) << spoils_of_war
+		end
+
+		if type == :mutually_assured_destruction
+			deck1.delete_at(0,1,2)
+			deck1.delete_at(0,1,2)
+		end
+	end
+
+
+end
+
+
---
+++ spec/turn_spec.rb
@@ -0,0 +1,36 @@
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/player.rb'
+require './lib/turn.rb'
+
+RSpec.configure do |config|
+config.formatter = :documentation
+end
+
+RSpec.describe Turn do
+  it "has a basic turn type" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  card4 = Card.new(:club, '5', 5)
+  card5 = Card.new(:heart, '8', 8)
+  card6 = Card.new(:diamond, 'Queen', 12)
+  card7 = Card.new(:heart, '3', 3)
+  card8 = Card.new(:diamond, '2', 2)
+
+  deck1 = Deck.new([card1, card2, card5, card8])
+  deck2 = Deck.new([card3, card4, card6, card7])
+
+  player1 = Player.new("Megan", deck1)
+  player2 = Player.new("Aurora", deck2)
+  
+
+
+  turn = Turn.new(player1, player2)
+
+
+  expect(turn.type).to eq(:basic)
+	expect(turn.winner).to eq(player2)
+  expect(turn.pile_cards).to eq([card1, card3])
+  end
+end
